e566d3687516d4eac07e81b4cb75741d2eb4a265
parthrana-27 <ranaparth296@gmail.com>
2025-06-16 12:27:19 +0000

    Code challenge submission for question: 704 Binary Search

---
+++ 704 Binary Search/704binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size(),l=0,r=n-1;
+       
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(target<nums[mid])
+            {
+                r=mid-1;
+            }
+            else if(target>nums[mid]){
+                l=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
