872b4024eca268697a4dfe44304af5c63cf1360d
parthrana-27 <ranaparth296@gmail.com>
2025-06-26 12:02:43 +0000

    Code challenge submission for question: 2311 Longest Binary Subsequence Less Than or Equal to K

---
+++ 2311 Longest Binary Subsequence Less Than or Equal to K/2311longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int n=s.length();
+        int zero=0,one=0;
+        long long val=0,pow=1;
+        for(char c:s){
+            if(c=='0') zero++;
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='1'){
+                if(val+pow>k){
+                    continue;
+                }
+                val+=pow;
+                one++;
+            }
+            pow<<=1;
+            if(pow>k) break;
+        }
+        return zero+one;
+    }
+};
