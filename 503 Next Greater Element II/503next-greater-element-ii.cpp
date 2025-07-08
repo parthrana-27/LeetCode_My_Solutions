7bcc85fd10fc8ccc5f901db558d34c885f42459b
parthrana-27 <ranaparth296@gmail.com>
2025-07-08 13:18:41 +0000

    Code challenge submission for question: 503 Next Greater Element II

---
+++ 503 Next Greater Element II/503next-greater-element-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+       int n=nums.size();
+       vector<int>ans(n);
+       stack<int>st;
+       for(int i=2*n-1;i>=0;i--)
+       {
+        while(!st.empty() && nums[i%n]>=nums[st.top()]) st.pop();
+        if(st.empty()) ans[i%n]=-1;
+        else ans[i%n]=nums[st.top()];
+        st.push(i%n);
+       }
+       return ans;
+    }
+};
