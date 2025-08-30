6345ef10f6a2ba0d6efb1db393a3865b35bde2c9
parthrana-27 <ranaparth296@gmail.com>
2025-08-30 07:53:49 +0000

    Code challenge submission for question: 36 Valid Sudoku

---
+++ 36 Valid Sudoku/36valid-sudoku.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9]={false};
+        bool cols[9][9]={false};
+        bool e[9][9]={false};
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    int num=board[i][j]-'1';
+                    int in=(i/3)*3+(j/3);
+                    if(rows[i][num] || cols[j][num] || e[in][num]) return false;
+                    rows[i][num]=cols[j][num] = e[in][num]=true;
+                }
+            }
+        }
+        return true;
+    }
+};
