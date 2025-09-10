4b9c65f1320da5cef75021d52fbc1e722555ec7f
parthrana-27 <ranaparth296@gmail.com>
2025-09-10 19:18:38 +0000

    Code challenge submission for question: 509 Fibonacci Number

---
+++ 509 Fibonacci Number/509fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+       if(n<=1) return n;
+       int prev2=0;
+       int prev=1;
+       for(int i=2;i<=n;i++)
+       {
+            int cur=prev2+prev;
+            prev2=prev;
+            prev=cur;
+       }
+       return prev;
+    }
+};
