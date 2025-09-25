02977b754b85ba12c533af0c20ac33ca7c5725eb
parthrana-27 <ranaparth296@gmail.com>
2025-09-25 03:13:10 +0000

    Code challenge submission for question: 680 Valid Palindrome II

---
+++ 680 Valid Palindrome II/680valid-palindrome-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool ispal(string &s,int l,int r){
+        while(l<r)
+        {
+            if(s[l]!=s[r]) return false;
+            l++,r--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+       int l=0,r=s.size()-1;
+       while(l<r)
+       {
+        if(s[l]!=s[r])
+        {
+            return ispal(s,l+1,r)||ispal(s,l,r-1);
+        }
+        l++,r--;
+       }
+       return true;
+    }
+};
