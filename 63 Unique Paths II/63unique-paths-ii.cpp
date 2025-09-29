8f8429215f527460329c9ce8263c3188a3d20827
parthrana-27 <ranaparth296@gmail.com>
2025-09-29 13:36:34 +0000

    Code challenge submission for question: 63 Unique Paths II

---
+++ 63 Unique Paths II/63unique-paths-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obs) {
+        int m=obs.size();
+        int n=obs[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=0 && j>=0 && obs[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[0][0]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
