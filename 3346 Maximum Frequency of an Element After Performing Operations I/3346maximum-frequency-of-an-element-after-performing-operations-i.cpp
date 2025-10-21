0b47168554ca468b8c35f63ec0ada68f2249dca4
parthrana-27 <ranaparth296@gmail.com>
2025-10-21 07:35:08 +0000

    Code challenge submission for question: 3346 Maximum Frequency of an Element After Performing Operations I

---
+++ 3346 Maximum Frequency of an Element After Performing Operations I/3346maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,22 @@
+class Solution {  
+public:  
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {  
+        int mx = *max_element(nums.begin(), nums.end());  
+        vector<int> freq(mx + 2), diff(mx + 2);  
+
+        for (auto& ele : nums) {  
+            freq[ele]++;  
+            diff[max(0, ele - k)]++;  
+            diff[min(mx, ele + k) + 1]--;  
+        }  
+
+        int mxFreq = 0, currFreq = 0;  
+        for (int i = 0; i <= mx; i++) {  
+            currFreq += diff[i];  
+            int finalOp = currFreq - freq[i];  
+            int freqPossible = finalOp > numOperations ? numOperations + freq[i] : currFreq;  
+            mxFreq = max(mxFreq, freqPossible);  
+        }  
+        return mxFreq;  
+    }  
+};  
