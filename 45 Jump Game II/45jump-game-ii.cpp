282582f826b7be8e0378965ad6cdd22a80dc3f61
parthrana-27 <ranaparth296@gmail.com>
2025-12-30 14:18:41 +0000

    Code challenge submission for question: 45 Jump Game II

---
+++ 45 Jump Game II/45jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int jump(vector<int>& nums) {
+        int l=0,r=0;
+        int n=nums.size();
+        int jumps=0;
+        while(r<n-1){
+        int far=0;
+        for(int ind=l;ind<=r;ind++){
+            far=max(far,ind+nums[ind]);
+        }
+        l=r+1;
+        r=far;
+        jumps++;
+        }
+        return jumps;
+    }
+};
