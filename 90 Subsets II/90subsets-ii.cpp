bea0e37e6b6b9a2ab9bc8eb28b0365b4d632401c
parthrana-27 <ranaparth296@gmail.com>
2026-01-10 10:12:02 +0000

    Code challenge submission for question: 90 Subsets II

---
+++ 90 Subsets II/90subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    private:
+    void f(int idx,vector<int>&nums,vector<int>temp,vector<vector<int>>&ans){
+        ans.push_back(temp);
+        for(int i=idx;i<nums.size();i++){
+            if(i!=idx &&nums[i]==nums[i-1])continue;
+            temp.push_back(nums[i]);
+            f(i+1,nums,temp,ans);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+        f(0,nums,temp,ans);
+        return ans;
+    }
+};
