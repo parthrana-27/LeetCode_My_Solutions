0ed6afafd56179424b3d13b3a0e437c2e34d1885
parthrana-27 <ranaparth296@gmail.com>
2026-01-12 19:34:52 +0000

    Code challenge submission for question: 516 Longest Palindromic Subsequence

---
+++ 516 Longest Palindromic Subsequence/516longest-palindromic-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    private:
+     int lcs(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<int>prev(m+1,0),cur(m+1,0);
+        for(int i=0;i<=m;i++) prev[i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]) cur[j]=1+prev[j-1];
+               else cur[j]=max(prev[j],cur[j-1]);
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return lcs(s,t);
+    }
+};
