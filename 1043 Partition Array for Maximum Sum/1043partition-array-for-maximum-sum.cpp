288cdefc149f26bbc9dde4b40f06c6b517d56e47
parthrana-27 <ranaparth296@gmail.com>
2026-01-22 17:31:51 +0000

    Code challenge submission for question: 1043 Partition Array for Maximum Sum

---
+++ 1043 Partition Array for Maximum Sum/1043partition-array-for-maximum-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k){
+        int n=arr.size();
+        vector<int>dp(n+1,0);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int len=0;
+            int ma=INT_MIN;
+            int ans=INT_MIN;
+            for(int j=i;j<min(i+k,n);j++){
+                len++;
+                ma=max(ma,arr[j]);
+                int sum=len*ma+dp[j+1];
+                ans=max(ans,sum);
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
+    }
+};
