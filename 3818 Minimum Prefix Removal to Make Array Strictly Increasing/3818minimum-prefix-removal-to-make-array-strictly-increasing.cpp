64d671ef8a3825ef1a9e48cf66f1fc196aad8466
parthrana-27 <ranaparth296@gmail.com>
2026-01-25 02:34:43 +0000

    Code challenge submission for question: 3818 Minimum Prefix Removal to Make Array Strictly Increasing

---
+++ 3818 Minimum Prefix Removal to Make Array Strictly Increasing/3818minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int n=nums.size();
+        int i=n-1;
+        while(i>0 && nums[i-1]<nums[i]){
+        i--;
+        }
+        return i;
+    }
+};
