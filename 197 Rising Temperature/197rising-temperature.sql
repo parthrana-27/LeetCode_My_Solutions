61f5abd7b04d0455ee40bfb27e5372c220a5edbc
parthrana-27 <ranaparth296@gmail.com>
2026-02-13 03:36:11 +0000

    Code challenge submission for question: 197 Rising Temperature

---
+++ 197 Rising Temperature/197rising-temperature.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select w1.id as Id from weather w1
+join weather w2 on datediff(w1.recorddate,w2.recorddate)=1
+where w1.temperature > w2.temperature
+order by w1.id;
