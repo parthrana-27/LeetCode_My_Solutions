44666835b3971200de2889798f14bd1f3baa88ab
parthrana-27 <ranaparth296@gmail.com>
2026-02-28 14:14:32 +0000

    Code challenge submission for question: 1680 Concatenation of Consecutive Binary Numbers

---
+++ 1680 Concatenation of Consecutive Binary Numbers/1680concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+       int mod=1e9+7;
+       long ans=0;
+       int bits=0;
+       for(int i=1;i<=n;i++){
+        if((i&(i-1))==0) bits++;
+        ans=((ans<<bits)%mod+i)%mod;
+       }
+       return ans;
+    }
+};
