1c2d0b8af3aa23d711682ee9619aa9f411f34399
parthrana-27 <ranaparth296@gmail.com>
2026-04-03 18:57:19 +0000

    Code challenge submission for question: 921 Minimum Add to Make Parentheses Valid

---
+++ 921 Minimum Add to Make Parentheses Valid/921minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' ) st.push(s[i]);
+            else if(s[i]==')' && !st.empty() && st.top()=='(') st.pop();
+            else st.push(s[i]);       
+        }
+        return st.size();
+    }
+};
