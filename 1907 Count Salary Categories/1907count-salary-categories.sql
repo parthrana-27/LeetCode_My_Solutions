49f84235d5f611ddb096d477db5bc8d8611d6a44
parthrana-27 <ranaparth296@gmail.com>
2026-05-16 12:57:12 +0000

    Code challenge submission for question: 1907 Count Salary Categories

---
+++ 1907 Count Salary Categories/1907count-salary-categories.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select 'Low Salary'as category,count(account_id) as accounts_count 
+from accounts where income < 20000
+union all
+select 'Average Salary'as category,count(account_id) as accounts_count 
+from accounts where income >= 20000 and income <= 50000
+union all
+select 'High Salary'as category,count(account_id) as accounts_count 
+from accounts where income > 50000;
