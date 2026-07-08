e1d8046f20f99618b5f00fc0b8400807b9c647df
parthrana-27 <ranaparth296@gmail.com>
2026-07-08 05:10:54 +0000

    Code challenge submission for question: 75 Sort Colors

---
+++ 75 Sort Colors/75sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,mid=0,h=nums.size()-1;
+        while(mid<=h){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[l]);
+                mid++;
+                l++;
+            }else if(nums[mid]==2){
+                swap(nums[mid],nums[h]);
+                h--;
+            }else{
+                mid++;
+            }
+        }
+    }
+};
